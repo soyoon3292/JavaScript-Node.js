c39784b01f1a0e3b49da7819f816d1a51c5b6a53
soyoon <soyoon@soyoon-MacBook-Air.local>
2018-12-07 01:02:07 +0900

    Installed npm package gulp, gulp-babel, @babel/core, @babel/preset-env.

--- Learning-JavaScript-macOS/gulpfile.js
+++ Learning-JavaScript-macOS/gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
-// 걸프 의존성을 여기 씁니다.
+const babel = require('gulp-babel');
 
-gulp.task('default', function() {
-	// 걸프 작업을 여기 씁니다.
+gulp.task('default', () => {
+    gulp.src("es6/**/*.js")
+    // .pipe(babel({ presets: ["@babel/env"] }))
+    .pipe(babel())
+    .pipe(gulp.dest("dist"));
+    gulp.src("public/es6/**/*.js")
+    // .pipe(babel({ presets: ["@babel/env"] }))
+    .pipe(babel())
+    .pipe(gulp.dest("public/dist"));
 });
