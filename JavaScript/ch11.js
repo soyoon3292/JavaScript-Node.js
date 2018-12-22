2c1272440b75fdd96c0e26705055f125a6a12690
soyoon <soyoon@soyoon-MacBook-Air.local>
2018-12-22 18:06:28 +0900

    Studied Chapter 11.

---
+++ JavaScript/ch11.js
@@ -0,0 +1,69 @@
+const err = new Error("invalid email");
+
+validateEmail = (email) => {
+    return email.match(/@/) ?
+        email :
+        new Error(`invalid email: ${email}`);
+}
+
+// const email = "jane@doe.com";
+
+// const validatedEmail = validateEmail(email);
+// if(validatedEmail instanceof Error) {
+//     console.error(`Error: ${validatedEmail.message}`);
+// } else {
+//     console.log(`Valid email: ${validatedEmail}`);
+// }
+
+
+// const email = null;
+
+// try {
+//     const validatedEmail = validateEmail(email);
+//     if(validatedEmail instanceof Error) {
+//         console.error(`Error: ${validatedEmail.message}`);
+//     } else {
+//         console.log(`Valid email: ${validatedEmail}`);
+//     }
+// } catch(err) {
+//     console.error(`Error: ${err.message}`);
+// }
+
+// billPay = (amount, payee, account) => {
+//     if(amount > account.balance)
+//         throw new Error("insufficient funds");
+//     account.transfer(payee, amount);
+// }
+
+function a() {
+    console.log("a: calling b");
+    b();
+    console.log("a: done");
+}
+function b() {
+    console.log("b: calling c");
+    c();
+    console.log("c: done");
+}
+function c() {
+    console.log("c: throwing error");
+    throw new Error("c error");
+    console.log("c: done");
+}
+function d() {
+    console.log("d: calling c");
+    c();
+    console.log("d: done");
+}
+
+try {
+    a();
+} catch(err) {
+    console.log(err.stack);
+}
+
+try {
+    d();
+} catch(err) {
+    console.log(err.stack);
+}
