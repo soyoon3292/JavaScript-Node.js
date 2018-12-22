b8404fe4827274c926f35cdde9b321cf1c40391b
soyoon <soyoon@soyoon-MacBook-Air.local>
2018-12-22 17:47:41 +0900

    Finished studying Chapter 10.

--- JavaScript/ch10.js
+++ JavaScript/ch10.js
@@ -1,24 +1,24 @@
-const u1 = { name: 'Cynthia' };
-const u2 = { name: 'Jackson' };
-const u3 = { name: 'Olive' };
-const u4 = { name: 'James' };
+// const u1 = { name: 'Cynthia' };
+// const u2 = { name: 'Jackson' };
+// const u3 = { name: 'Olive' };
+// const u4 = { name: 'James' };
 
-// const userRoles = new Map();
+// // const userRoles = new Map();
 
-// userRoles.set(u1, 'User');
-// userRoles.set(u2, 'User');
-// userRoles.set(u3, 'Admin');
+// // userRoles.set(u1, 'User');
+// // userRoles.set(u2, 'User');
+// // userRoles.set(u3, 'Admin');
 
-// userRoles
-//     .set(u1, 'User')
-//     .set(u2, 'User')
-//     .set(u3, 'Admin');
+// // userRoles
+// //     .set(u1, 'User')
+// //     .set(u2, 'User')
+// //     .set(u3, 'Admin');
 
-const userRoles = new Map([
-    [u1, 'User'],
-    [u2, 'User'],
-    [u3, 'Admin']
-]);
+// const userRoles = new Map([
+//     [u1, 'User'],
+//     [u2, 'User'],
+//     [u3, 'Admin']
+// ]);
 
 // console.log(userRoles.get(u2));
 // console.log(userRoles.has(u1));
@@ -47,10 +47,63 @@ const userRoles = new Map([
 // for(let [u, r] of userRoles) // entries() 메서드는 맵의 기본 이터레이터, 생략 가능하다
 //     console.log(`${u.name}: ${r}`);
 
-console.log([...userRoles.values()]);
+// console.log([...userRoles.values()]);
 
-userRoles.delete(u2);
-userRoles.size;
+// userRoles.delete(u2);
+// console.log(userRoles.size);
 
-userRoles.clear;
-userRoles.size;
+// userRoles.clear();
+// console.log(userRoles.size);
+
+
+// const SecretHolder = (() => {
+//     const secrets = new WeakMap();
+//     return class {
+//         setSecret(secret) {
+//             secrets.set(this, secret);
+//         }
+//         getSecret() {
+//             return secrets.get(this);
+//         }
+//     }
+// })();
+
+// const a = new SecretHolder();
+// const b = new SecretHolder();
+
+// a.setSecret('secret A');
+// b.setSecret('secret B');
+
+// console.log(a.getSecret());
+// console.log(b.getSecret());
+
+
+// const roles = new Set();
+
+// roles.add('User');
+// roles.add('Admin');
+// console.log(roles.size);
+
+// roles.add('User');
+// console.log(roles.size);
+
+// console.log(roles.delete('Admin'));
+// console.log(roles);
+// console.log(roles.delete('Admin'));
+
+
+const naughty = new WeakSet();
+
+const children = [
+    { name: 'Suzy' },
+    { name: 'Derek' }
+];
+
+naughty.add(children[1]);
+
+for(let child of children) {
+    if(naughty.has(child))
+        console.log(`Coal for ${child.name}!`);
+    else
+        console.log(`Presents for ${child.name}!`);
+}
