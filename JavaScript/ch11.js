1d0ef8205767c48edde50fa2e8fb9b0af5a70480
soyoon3292 <jsp0916@gmail.com>
2018-12-23 15:24:36 +0900

    Finished studying Chapter 11.

--- JavaScript/ch11.js
+++ JavaScript/ch11.js
@@ -1,10 +1,10 @@
-const err = new Error("invalid email");
+// const err = new Error("invalid email");
 
-validateEmail = (email) => {
-    return email.match(/@/) ?
-        email :
-        new Error(`invalid email: ${email}`);
-}
+// validateEmail = (email) => {
+//     return email.match(/@/) ?
+//         email :
+//         new Error(`invalid email: ${email}`);
+// }
 
 // const email = "jane@doe.com";
 
@@ -35,35 +35,49 @@ validateEmail = (email) => {
 //     account.transfer(payee, amount);
 // }
 
-function a() {
-    console.log("a: calling b");
-    b();
-    console.log("a: done");
-}
-function b() {
-    console.log("b: calling c");
-    c();
-    console.log("c: done");
-}
-function c() {
-    console.log("c: throwing error");
-    throw new Error("c error");
-    console.log("c: done");
-}
-function d() {
-    console.log("d: calling c");
-    c();
-    console.log("d: done");
-}
+
+// function a() {
+//     console.log("a: calling b");
+//     b();
+//     console.log("a: done");
+// }
+// function b() {
+//     console.log("b: calling c");
+//     c();
+//     console.log("c: done");
+// }
+// function c() {
+//     console.log("c: throwing error");
+//     throw new Error("c error");
+//     console.log("c: done");
+// }
+// function d() {
+//     console.log("d: calling c");
+//     c();
+//     console.log("d: done");
+// }
+
+// try {
+//     a();
+// } catch(err) {
+//     console.log(err.stack);
+// }
+
+// try {
+//     d();
+// } catch(err) {
+//     console.log(err.stack);
+// }
+
 
 try {
-    a();
+    console.log("this line is executed...");
+    throw new Error("whoops");
+    console.log("this line is not...");
 } catch(err) {
-    console.log(err.stack);
-}
-
-try {
-    d();
-} catch(err) {
-    console.log(err.stack);
+    console.log("there was an error...");
+    console.error(err.message);
+} finally {
+    console.log("...always executed");
+    console.log("perform cleanup here");
 }
